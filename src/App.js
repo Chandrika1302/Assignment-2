60cefc94dcea54856402987a2170bae92d0da198
chandrika <gehini1302@gmail.com>
2022-07-01 17:50:21 +0530

    add movieCards

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
+import MovieCard from "./components/MovieCard";
+import data from "./data";
 
 function App() {
+  const items = data;
   return (
     <div className="App">
-      hi
+      <header> 
+        <h1>Movies</h1>
+      </header>
+      <div className="movie-grid">
+        {items.map((item) => (
+          <MovieCard key={item.id} movie={item} />
+        ))}
+      </div>
     </div>
   );
 }
